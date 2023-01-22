8749f0d2cf87bbb45bcb85547f6864b2c8f8e0a8
aiscodes <aigerim6769@gmail.com>
2023-01-22 22:36:28 +0600

    :bug: add new command to config script

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,17 +1,17 @@
 // @ts-nocheck
 const { test, expect, chromium } = require('@playwright/test')
 
-// test('get started', async ({ page }) => {
-//   // await page.goto('http://localhost:3000/')
+test('get started', async ({ page }) => {
+  // await page.goto('http://localhost:3000/')
 
-//   await page.getByPlaceholder('find').fill('avatar')
-//   await page.getByRole('button', { name: /search/i }).click()
-//   await expect(page.getByText('Movie bar')).toBeVisible()
-//   const header = page.locator('#header')
-//   await expect(header).toHaveClass('header__center')
-//   const main = page.locator('#main')
-//   await expect(main).toHaveClass('main')
-// })
+  await page.getByPlaceholder('find').fill('avatar')
+  await page.getByRole('button', { name: /search/i }).click()
+  await expect(page.getByText('Movie bar')).toBeVisible()
+  const header = page.locator('#header')
+  await expect(header).toHaveClass('header__center')
+  const main = page.locator('#main')
+  await expect(main).toHaveClass('main')
+})
 
 test('check request and response', async () => {
   const browser = await chromium.launch()
